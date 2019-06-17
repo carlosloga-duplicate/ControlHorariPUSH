b5bf7df4db7b7cab21899481e66d83f9d1125c43
carlosloga-duplicate <carlosloga@protonmail.com>
2019-06-17 12:31:28 +0200

    V21

--- www/js/index.js
+++ www/js/index.js
@@ -28,23 +28,15 @@ var app = {
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-/*         console.log('Received Device Ready Event');
-        console.log('calling setup push'); */
-        app.setupPush();
 
         setTimeout("$.mobile.changePage('#pageSETHORA', { transition: 'slideup', changeHash: false });", 2000);
 
+        $('body').on('click','imgENTRADA',function(){Confirmar('ENTRADA');})
+        $('body').on('click','imgSORTIDA',function(){Confirmar('SORTIDA');})
+
         /* Informar data actual */
-        var today = new Date().toString();   
- /*        var dd = today.getDate().toString();
-        var mm = (today.getMonth()+1).toString(); //January is 0!
-        var yyyy = today.getFullYear().toString(); 
-        var queryDate = new Date(today.getFullYear(),today.getMonth(),today.getDate());  */  
+        var today = new Date().toString();    
         $("input[type=date]").val(today);              /* yyyy-MM-dd  */
 
         /* acceder al combo que hay dentro del radioButton */
@@ -80,58 +72,7 @@ var app = {
             }
         }); 
 
-        $('body').on('click','imgENTRADA',function(){Confirmar('ENTRADA');})
-        $('body').on('click','imgSORTIDA',function(){Confirmar('SORTIDA');})
-
-    },
-    setupPush: function() {
-        console.log('calling push init');
-        var push = PushNotification.init({
-            "android": {
-                "senderID": "XXXXXXXX"
-            },
-            "browser": {},
-            "ios": {
-                "sound": true,
-                "vibration": true,
-                "badge": true
-            },
-            "windows": {}
-        });
-        console.log('after init');
-
-        push.on('registration', function(data) {
-            console.log('registration event: ' + data.registrationId);
-
-            var oldRegId = localStorage.getItem('registrationId');
-            if (oldRegId !== data.registrationId) {
-                // Save new registration ID
-                localStorage.setItem('registrationId', data.registrationId);
-                // Post registrationId to your app server as the value has changed
-            }
-
-            var parentElement = document.getElementById('registration');
-            var listeningElement = parentElement.querySelector('.waiting');
-            var receivedElement = parentElement.querySelector('.received');
-
-            listeningElement.setAttribute('style', 'display:none;');
-            receivedElement.setAttribute('style', 'display:block;');
-        });
-
-        push.on('error', function(e) {
-            console.log("push error = " + e.message);
-        });
-
-        push.on('notification', function(data) {
-            console.log('notification event');
-            navigator.notification.alert(
-                data.message,         // message
-                null,                 // callback
-                data.title,           // title
-                'Ok'                  // buttonName
-            );
-       });
-    }
+    }   
 };
 
 function Confirmar(sQue)
