71d65acd3bbe4b4473941feafaf2b4c5a51f51fd
carlosloga-duplicate <carlosloga@protonmail.com>
2019-06-27 17:37:49 +0200

    v.360

--- www/js/Notificacions.js
+++ www/js/Notificacions.js
@@ -1,7 +1,9 @@
 
+/* FAQS : https://github.com/katzer/cordova-plugin-local-notifications/issues?page=4&q=is%3Aissue+is%3Aopen */
+
 /* https://github.com/sitepoint-editors/ReminderApp */
 
-function configurarNotificacio(titol, missatge, quan, cadaMinutos, id)
+/* function configurarNotificacio(titol, missatge, quan, cadaMinutos, id)
 {
     var sound = device.platform == 'Android' ? 'file://sound.mp3' : 'file://beep.caf';
     var date = new Date();
@@ -24,7 +26,7 @@ function configurarNotificacio(titol, missatge, quan, cadaMinutos, id)
         sound: sound,
         icon: "http://domain.com/icon.png"
     });
-}
+} */
 
 function crearNotificacio(id, date, hora, titulo, msg)
 {   
@@ -133,7 +135,7 @@ alert('granted OK');
     }
 }
 
-function ____crearNotificacio(id, date, time, title, message)
+/* function ____crearNotificacio(id, date, time, title, message)
 {
 //alert('en crearNotificacio: ' + date + '|' + time + '|' + title + '|' + message);  
     if(date == "" || time == "" || title == "" || message == "")
@@ -142,18 +144,18 @@ function ____crearNotificacio(id, date, time, title, message)
       return;
     }
 
-/*     var schedule_time = new Date((date + " " + time).replace(/-/g, "/")).getTime();
-alert('schedule_time = ' + schedule_time.toString());    
-    schedule_time = new Date(schedule_time);
-alert('schedule_time = ' + schedule_time.toString()); 
- */
+//     var schedule_time = new Date((date + " " + time).replace(/-/g, "/")).getTime();
+//  alert('schedule_time = ' + schedule_time.toString());    
+//    schedule_time = new Date(schedule_time);
+// alert('schedule_time = ' + schedule_time.toString()); 
+
 
     var now = new Date().getTime();
     var schedule_time = new Date(now + 60*2000); //2 mi. más 
 alert('schedule_time = ' + schedule_time.toString()); 
 
-/*     cancelarNotificacio(1);
-    cancelarNotificacio(2); */
+   // cancelarNotificacio(1);
+   // cancelarNotificacio(2); 
 
     cordova.plugins.notification.local.hasPermission(function(granted){
       if(granted == true)
@@ -181,9 +183,9 @@ alert('schedule_time = ' + schedule_time.toString());
         });
       }
     });
-}
+} */
 
-function schedule(id, title, message, schedule_time)
+/* function schedule(id, title, message, schedule_time)
 {
 alert('en shedule');  
     cordova.plugins.notification.local.schedule({
@@ -191,19 +193,19 @@ alert('en shedule');
         title: title,
         message: message,
         at: schedule_time
-    });
+    }); */
 
 /*     var array = [id, title, message, schedule_time];
     info.data[info.data.length] = array;
-    localStorage.setItem("rp_data", JSON.stringify(info)); */
+    localStorage.setItem("rp_data", JSON.stringify(info)); 
     alert("L'avís s'ha creat correctament");
     mensajePopup("OK", "L'avís s'ha creat correctament",2000); 
-}
+}  */
 
-function cancelarNotificacio(id)
+/* function cancelarNotificacio(id)
 {
     cordova.plugins.notification.local.cancel(id, function () {
         // Notification was cancelled
     }, scope);
-}
+} */
 
